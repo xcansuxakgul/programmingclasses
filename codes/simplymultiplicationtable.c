4a230fb99cce0ac6ae98109d7f1a4245abc44add
Emine Cansu Akgül <eminecansuakgul@gmail.com>
2022-01-06 11:17:03 +0300

    Create simplymultiplicationtable.c

---
+++ codes/simplymultiplicationtable.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int rows,columns,i,j;
+	
+	printf("enter columns and rows");
+	scanf("%d%d",&columns,&rows);
+	
+	for(i=1;i<=rows;i++){
+		for(j=1;j<=columns;j++){
+			printf("%3d",i*j);
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
