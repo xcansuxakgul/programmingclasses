1c22c3f0e4980ee67a377d2fe582579bbc7791df
Emine Cansu Akgül <eminecansuakgul@gmail.com>
2022-01-06 12:25:39 +0300

    Update recursivefactorial.c

--- codes/recursivefactorial.c
+++ codes/recursivefactorial.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int factorial(int n) {
+   //base case
+   if(n == 0) {
+      return 1;
+   } else {
+      return n * factorial(n-1);
+   }
+}
+
+int main() {
+
+   	int n,i;
+	printf("Enter a number to calculate the factorial of its");
+	scanf("%d",&n);
+	printf("Factorial of %d: %d\n" , n , factorial(n));
+	return 0;
+}
+
+// previous my inefficient code 
+
+/*#include <stdio.h>
+#include <stdlib.h>
+
 int factor(int num,int fact){
-	if (num == 1) return 1;
-  fact *= num;
+	if (num == 1 && num == 0) return 1;
+  	fact *= num;
 	if ((num-1) ==1)return fact;
 	return factor(num-1, fact);
 }
@@ -16,4 +39,4 @@ int main() {
 	printf("\nThe result is %d\n", factor(num,fact));
 	
 	return 0;
-}
+}*\
